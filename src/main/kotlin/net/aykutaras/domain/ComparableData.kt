761ef678b59ab8ef7fb56fa5551f7a214e979d63
Aykut Aras <aykutaras@gmail.com>
2018-02-23 12:58:44 +0300

    Domain event tests done

--- src/main/kotlin/net/aykutaras/domain/ComparableData.kt
+++ src/main/kotlin/net/aykutaras/domain/ComparableData.kt
@@ -5,24 +5,28 @@ data class ComparableData(
         private var right: String? = null,
         private var locked: Boolean = false
 ) {
-    fun addDataToLeft(data: String) {
+    fun addDataToLeft(data: String): ComparableData {
         checkIfLocked()
 
         this.left = data
+        return this
     }
 
-    fun addDataToRight(data: String) {
+    fun addDataToRight(data: String): ComparableData {
         checkIfLocked()
 
         this.right = data
+        return this
     }
 
-    fun lock() {
+    fun lock(): ComparableData {
         this.locked = true
+        return this
     }
 
-    fun unlock() {
+    fun unlock(): ComparableData {
         this.locked = false
+        return this
     }
 
     private fun checkIfLocked() {
--- src/test/groovy/net/aykutaras/domain/ComparableDataSpec.groovy
+++ src/test/groovy/net/aykutaras/domain/ComparableDataSpec.groovy
@@ -1,4 +1,45 @@
 package net.aykutaras.domain
 
-class ComparableDataSpec {
+import spock.lang.Specification
+
+class ComparableDataSpec extends Specification {
+  def "Should add data to left and right"() {
+    given:
+    def data = new ComparableData()
+
+    when:
+    data.addDataToLeft("Left Data")
+        .addDataToRight("Right Data")
+
+    then:
+    data.left == "Left Data"
+    data.right == "Right Data"
+  }
+
+  def "Should throw exception when adding locked data"() {
+    given:
+    def data = new ComparableData()
+    data.lock()
+
+    when:
+    data.addDataToLeft("Left Data")
+
+    then:
+    thrown IllegalStateException
+  }
+
+  def "Should add data locked data when unlocked"() {
+    given:
+    def data = new ComparableData()
+    data.addDataToRight("Sample Data")
+    .lock()
+    .unlock()
+
+    when:
+    data.addDataToLeft("Sample Data")
+
+    then:
+    data.left == "Sample Data"
+    data.right == "Sample Data"
+  }
 }
